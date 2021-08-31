e828cc2ba67db71aea4e113af344c06021533ecc
Audrey Mengue <massoumeharmonie@gmail.com>
2021-08-31 23:53:45 +0000

    Added some logic

---
+++ backgroundImageSlider/script.js
@@ -0,0 +1,28 @@
+(function() {
+
+const pictures = ["car1", "car2", "car3", "car4"];
+
+
+const buttons = document.querySelectorAll('.btn')
+  const imageDIV = document.querySelector('.slider')
+  let counter = 0
+
+  buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+      if (button.classList.contains('btn-left')){
+        counter--
+        if(counter < 0){
+          counter = pictures.length -1
+        }
+        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+      }
+      if (button.classList.contains('btn-right')){
+        counter++
+        if(counter > pictures.length -1){
+          counter = 0
+        }
+        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+      }
+    })
+  })
+})();
